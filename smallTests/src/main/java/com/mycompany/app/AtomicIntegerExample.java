0e623b19022377bf5b0e726fc3a550699dcd8323
JD <me@ww.pl>
2016-09-24 14:54:26 +0200

    AtomicInteger example

---
+++ smallTests/src/main/java/com/mycompany/app/AtomicIntegerExample.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerExample {
+
+    public static void main(String[] args) {
+
+        IntegerValues values = new IntegerValues();
+        new MyThread(values).start();
+        new MyThread(values).start();
+    }
+
+    static class IntegerValues {
+        AtomicInteger value1 = new AtomicInteger(0);
+        Integer value2 = new Integer(0);
+
+        public void incrementAndPrint() {
+            System.out.println(String.format("Atomic: %s   Regular: %s", value1.incrementAndGet(), ++value2));
+        }
+    }
+
+    static class MyThread extends Thread {
+
+        private IntegerValues integerValues;
+
+        public MyThread(IntegerValues integerValues1) {
+            this.integerValues = integerValues1;
+        }
+
+        @Override
+        public void run() {
+            for(int i=0;i<100000;i++)
+                integerValues.incrementAndPrint();
+        }
+    }
+
+}
