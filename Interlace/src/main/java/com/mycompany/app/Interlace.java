cf3f096279d21f6a6166db10d6be6c5ed9fe9197
JD <me@ww.pl>
2016-09-24 12:16:43 +0200

    removed unnecessary lines

--- Interlace/src/main/java/com/mycompany/app/Interlace.java
+++ Interlace/src/main/java/com/mycompany/app/Interlace.java
@@ -4,8 +4,8 @@ import com.beust.jcommander.internal.Lists;
 import com.google.common.primitives.Chars;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Random;
 
 public class Interlace {
     ArrayList<Character> interlacedArray = new ArrayList();
@@ -30,65 +30,6 @@ public class Interlace {
     private void addToArrayIfNotNull(Character character) {
         if (character != null) {
             interlacedArray.add(character);
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] array = {
-                0b10111111,
-                0b1101,
-                0xF,
-                0b11 << 2,
-                0b11111111 >> 1,
-                0b11111111,
-                0b10111010 >> 4,
-                0b10111010 >>> 4,
-                0b10111111
-        };
-        for (int a : array) {
-            System.out.println(a);
-        }
-
-        System.out.println("Thread!! Name:" + Thread.currentThread().getName());
-        new MyThread().start();
-        new MyThread().start();
-
-    }
-
-    static class MyThread extends Thread {
-
-        public static final int NUM = 10;
-        ThreadLocal<Integer> value = new ThreadLocal<>();
-        Integer globalValue = 1;
-
-        public MyThread() {
-            value.set(0);
-        }
-
-        @Override
-        public void run() {
-            value.set(0);
-            for (int i = 0; i < NUM; i++) {
-                value.set(value.get()+1);
-                globalValue++;
-                logMessage("VALUE: "+value.get() + " GLOBAL: " +globalValue);
-                try {
-                    Thread.sleep(new Random().nextInt(10)*100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        private void logMessage(String msg) {
-            System.out.println("MyThread: Name:" + Thread.currentThread().getName() + "-> Message: " + msg);
-        }
-    }
-
-    static class MyRunnable implements Runnable {
-        @Override
-        public void run() {
-            System.out.println("MyRunnable!! Name:" + Thread.currentThread().getName());
         }
     }
 
